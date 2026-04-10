17109189e0b31f955debb3af366ba1e6a20a8bc1
Sam Tomashi <samtomashikayembi@yhaoo.fr>
2026-04-10 15:43:44 +0300

    update product

--- js/index.js
+++ js/index.js
@@ -1,19 +1,59 @@
-/**
- * The 3 pillars of JavaScript:
- * 1. Handling Events: Event listeners(onclick, onsubmit, input, onchange, onkeyup, etc.)
- * 2. DOM(Document Object Model: It's a representation of HTML elements as objects in Javascript) Manipulation:
- * 3. Communicating with the server
- */
+// const { createElement } = require("react")
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", async function(){
   const frm = document.querySelector("#frm-product")
   handleSubmit(frm)
-  getProducts()
+  const products = await getProducts()
+
+
+  displayProducts(products)
+  populateProducts(products)
+
+
+  handleProductChange()
+
+  updateProduct()
+
 
 })
 
-function getProducts(){
-  fetch("http://localhost:3001/products", {
+function populateProductForm(product){
+  const frmUpdate = document.querySelector("#frm-update-product")
+  // const formData = new FormData(frmUpdate)
+  // formData.set("name", product.name)
+  // formData.set("price", product.price)
+  // formData.set("imageUrl", product.imageUrl)
+
+  frmUpdate.name.value = product.name
+   frmUpdate.price.value = product.price
+    frmUpdate.imageUrl.value = product.imageUrl
+
+
+}
+
+
+function handleProductChange(){
+    const productSelectBox = document.querySelector("#productSelectBox")
+    productSelectBox.addEventListener("change", async function(event){
+      const product = await getProduct(event.target.value)
+      populateProductForm(product)
+    })
+
+}
+ 
+
+function populateProducts(products){
+    const productSelectBox = document.querySelector("#productSelectBox")
+
+    products.forEach((product)=> {
+      productSelectBox.innerHTML += `
+              <option value="${product.id}">${product.name}</option>
+  `
+    })
+}
+
+function getProduct(id){
+  return fetch(`http://localhost:3003/products/${id}`, {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
@@ -21,7 +61,17 @@ function getProducts(){
   }
 })
   .then((response) => response.json())
-  .then((data) => displayProducts(data));
+}
+
+function getProducts(){
+  return fetch("http://localhost:3003/products", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+})
+  .then((response) => response.json())
 }
 
 function displayProducts(data) {
@@ -35,6 +85,7 @@ function displayProducts(data) {
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text">${product.category}</p>
                     <p class="card-text">${product.brand}</p>
+                    <p class="card-text">${product.price} Ksh</p>
                     <a href="#" class="btn btn-sm btn-danger">Delete Product</a>
                 </div>
             </div>
@@ -45,7 +96,6 @@ function displayProducts(data) {
 function handleSubmit(frm){
   frm.addEventListener("submit", (event)=> {
     event.preventDefault();
-
     const product = {
       name: event.target.name.value,
       price: event.target.price.value,
@@ -60,7 +110,7 @@ function handleSubmit(frm){
 }
 
 function createProduct(product){
-fetch("http://localhost:3001/products", {
+fetch("http://localhost:3003/products", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -68,6 +118,25 @@ fetch("http://localhost:3001/products", {
       },
       body: JSON.stringify(product)
     })
+}
+
+function updateProduct(){
+  const frm = document.querySelector("#frm-update-product")
+  frm.addEventListener("submit", function(event){
+    const id = event.target.products
+    fetch(`http://localhost:3003/products/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: {
+        name: event.target.name,
+        price: event.target.price,
+        imageUrl: event.target.imageUrl
+      }
+    })
+  })
 }
 
 
